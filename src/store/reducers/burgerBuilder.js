c48101bdfb9933c18b18beafdfc7c7704f2e1532
Tolik Dem <tolik.demianov@gmail.com>
2018-07-13 19:21:38 +0300

    устранил ошибку при которой цена НЕ обновлялась после редиректа

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -44,6 +44,7 @@ const reducer = (state = initialState, action) => {
           cheese: action.ingredients.cheese,
           meat: action.ingredients.meat,
         },
+        totalPrice: 0,
         loadingError: false,
       };
     case actionTypes.FETCH_INGREDIENTS_FAILED:
