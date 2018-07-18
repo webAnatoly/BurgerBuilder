09b9a3e2e155bbae51f88c3f62bdc82d179f970e
Tolik Dem <tolik.demianov@gmail.com>
2018-07-18 22:42:11 +0300

    Rename menu item

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const NavigationItems = () => (
   <ul className={s.NavigationItems}>
-    <NavigationItem link="/" exact>Создать Бутерброд</NavigationItem>
+    <NavigationItem link="/" exact>Наполнить бутерброд</NavigationItem>
     <NavigationItem link="/orders">Заказы</NavigationItem>
     <NavigationItem link="/auth">Авторизация</NavigationItem>
     {/* <NavigationItem link="/checkout">Checkout</NavigationItem> */}
