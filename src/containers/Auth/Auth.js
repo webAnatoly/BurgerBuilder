8561703aaa6e436b2e6c09a965c1df43ae4f8b97
Tolik Dem <tolik.demianov@gmail.com>
2018-07-19 16:09:15 +0300

    Добавил две функции в Auth.js
    * Функция inputChangedHandler() для обработки пользовательского ввода
    * Функиция checkValidity() для валидации пользовательского ввода

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -32,6 +32,37 @@ class Auth extends React.Component {
       },
     };
   }
+
+  checkValidity = (value, rules) => {
+    let isValid = true;
+
+    if (rules) {
+      if (rules.required) {
+        isValid = value.trim() !== '' && isValid;
+      }
+      if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+      }
+      if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid;
+      }
+    }
+    return isValid;
+  }
+
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+        touched: true, // флаг для определения был ли ввод в поле от пользователя
+      },
+    };
+    this.setState({ controls: updatedControls });
+  }
+
   render() {
     const formElementsArray = [];
     // Заполняем массив объектами конфигурации на основе которых будут созданы инпуты в форме
