636e56c6f059d778a599dc3f0406144426546762
Tolik Dem <tolik.demianov@gmail.com>
2018-07-24 17:11:48 +0300

    Изменил атрибут кнопки

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -146,7 +146,7 @@ class ContactData extends React.Component {
             changed={event => this.inputChangedHandler(event, formElement.id)}
           />))}
         <Button
-          type="button"
+          type="submit"
           btnType="Success"
           disabled={!this.state.formIsValid} // disable is true if the form is not valid
         >
