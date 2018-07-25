1a9dabdc486ff6faa85f8ce5252e8bcbbfc3c6b5
Tolik Dem <tolik.demianov@gmail.com>
2018-07-25 18:25:24 +0300

    Реализовал перенаправление на главную
    * Если неавторизованый пользователь переходит по адресу http://localhost:8080/orders то пападает на главную. Ну и вообще если любой пользоваетль в url введет некоректный адрес то будет перенаправлен на главную.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
@@ -20,21 +20,39 @@ class App extends React.Component {
     this.props.onTryAutoSignUp();
   }
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" component={BurgerBuilder} />
+        {/* Если юзер запросит несуществующею страницу, перенаправить его на главную */}
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" component={BurgerBuilder} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.token !== null,
+});
 
 const mapDispatchToProps = dispatch => ({
   onTryAutoSignUp: () => dispatch(actions.authCheckState()),
@@ -42,6 +60,7 @@ const mapDispatchToProps = dispatch => ({
 
 App.propTypes = {
   onTryAutoSignUp: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired,
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
