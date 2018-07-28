4681f333cd9e4d8521955a2db0431b7a8eed743b
Tolik Dem <tolik.demianov@gmail.com>
2018-07-28 12:18:40 +0300

    добавил пару условий в функцию валидации checkValidity

--- src/share/checkValidity.js
+++ src/share/checkValidity.js
@@ -2,6 +2,7 @@
 Принимает значение которое нужно проверить и правила валидации в виде объекта. */
 export default (value, rules) => {
   let isValid = true;
+  if (!rules) { return true; }
 
   if (rules) {
     if (rules.required) {
@@ -12,6 +13,13 @@ export default (value, rules) => {
     }
     if (rules.maxLength) {
       isValid = value.length <= rules.maxLength && isValid;
+    }
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+    if (rules.isNaturalNumber) {
+      isValid = /^\d+$/.test(value) && isValid;
     }
   }
 
